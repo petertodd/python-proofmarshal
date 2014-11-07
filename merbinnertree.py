d768cba5208196699d61c5f7d53b560f935560e3
Peter Todd <pete@petertodd.org>
2014-11-07 10:04:29 -0500

    Add merbinnertree (de)serialization and hashing
    Non-prunable version for ease of implementation by others; serialization
    and hashing should match a future prunable version.

--- merbinnertree.py
+++ merbinnertree.py
@@ -9,7 +9,11 @@
 # propagated, or distributed except according to the terms contained in the
 # LICENSE file.
 
+import hashlib
+import hmac
 import operator
+
+import proofmarshal
 
 def calc_summed_merbinner_hash(items, *,
                                hash_func=None,
@@ -75,3 +79,116 @@ def calc_merbinner_hash(items, *, hash_func=None, _depth=0):
     return calc_summed_merbinner_hash(((k, v, 0) for k,v in items),
                                       hash_func=hash_func,
                                       sum_serialize_func=lambda s: b'')[0]
+
+class MerbinnerTree(proofmarshal.ImmutableProof, dict):
+    """Temporary non-prunable merbinner tree
+
+    To reduce implementation effort for the Java team.
+    """
+    HASH_HMAC_KEY = None
+
+    SUM_IDENTITY = 0
+
+    key_serialize = None
+    key_deserialize = None
+    value_serialize = None
+    value_deserialize = None
+    sum_serialize = lambda self, ctx, sum: None
+    sum_deserialize = lambda self, ctx, sum: 0
+
+    key_gethash = lambda self, key: key.hash
+    value_gethash = lambda self, value: value.hash
+    value_getsum = lambda self, value: 0
+
+    sum_func = operator.add
+
+    def _ctx_serialize(self, ctx):
+        def recurse(ctx, items, depth):
+            if len(items) == 0:
+                # Empty node
+                ctx.write_varuint('type', 0)
+                return self.SUM_IDENTITY
+
+            elif len(items) == 1:
+                # Leaf node
+                ctx.write_varuint('type', 1)
+
+                key, value, sum = items[0]
+                self.key_serialize(ctx, key)
+                self.value_serialize(ctx, value)
+
+                return sum
+
+            else:
+                # Inner node
+                ctx.write_varuint('type', 2)
+
+                # Sort items into left and right sides
+                left_items = []
+                right_items = []
+                for key, value, sum in items:
+                    # FIXME: detect dup keys here
+                    keyhash = self.key_gethash(key)
+                    side = key[depth // 8] >> (7 - depth % 8) & 0b1
+
+                    if side:
+                        left_items.append((key, value, sum))
+                    else:
+                        right_items.append((key, value, sum))
+
+                # Definitely a hack, but if we're hashing, rather than
+                # serializing, we need to create new contexts and do the hmac
+                # hash calculation. This hack will get removed when the full
+                # prunable merbinner tree is implemented.
+                def do_recurse(items):
+                    sum = None
+                    if isinstance(ctx, proofmarshal.HashSerializationContext):
+                        next_ctx = proofmarshal.HashSerializationContext()
+                        sum = recurse(next_ctx, items, depth+1)
+                        hash = hmac.HMAC(self.HASH_HMAC_KEY, next_ctx.getbytes(), hashlib.sha256).digest()
+                        ctx.write_bytes(None, hash, 32)
+
+                        # Also, sum only needs to be serialized while hashing;
+                        # serializing it otherwise is redundent as the sums can
+                        # be recalculated from the values. (future pruned nodes
+                        # will have a sum field)
+                        self.sum_serialize(ctx, sum)
+                    else:
+                        sum = recurse(ctx, items, depth+1)
+
+                    return sum
+
+                left_sum = do_recurse(left_items)
+                right_sum = do_recurse(right_items)
+
+                return self.sum_func(left_sum, right_sum)
+
+        items = [(key, value, self.value_getsum(value)) for key, value in self.items()]
+
+        final_sum = recurse(ctx, items, 0)
+
+    def _ctx_deserialize(self, ctx):
+        items = {}
+        def recurse():
+            node_type = ctx.read_varuint('type')
+
+            if node_type == 0:
+                # Empty node, do nothing
+                pass
+
+            elif node_type == 1:
+                # Leaf node
+                key = self.key_deserialize(ctx)
+                value = self.value_deserialize(ctx)
+
+                self[key] = value
+
+            elif node_type == 2:
+                # Inner node
+                recurse() # left
+                recurse() # right
+
+            else:
+                raise Exception('unsupported node type: %d' % node_type)
+
+        recurse()
--- test/test_merbinnertree.py
+++ test/test_merbinnertree.py
@@ -15,81 +15,148 @@ import json
 import os
 import struct
 import unittest
+import uuid
+
+from proofmarshal.test import *
 
 from proofmarshal.merbinnertree import *
 
-def x(h):
-    h = h.replace(' ','')
-    return binascii.unhexlify(h.encode('utf8'))
+class BytesBytesMerbinnerTree(MerbinnerTree):
+    HASH_HMAC_KEY = x('92e8898fcfa8b86b60b32236d6990da0')
 
-def b2x(b):
-    return binascii.hexlify(b).decode('utf8')
+    KEY_LENGTH = 4
+    VALUE_LENGTH = 4
 
-def load_test_vectors(name):
-    with open(os.path.dirname(__file__) + '/data/' + name, 'r') as fd:
-        for test_case in json.load(fd):
-            yield test_case
+    key_serialize = lambda self, ctx, key: ctx.write_bytes('key', key, self.KEY_LENGTH)
+    key_deserialize = lambda self, ctx: ctx.read_bytes('key', self.KEY_LENGTH)
 
-class Test_calc_merbinner_hash(unittest.TestCase):
+    value_serialize = lambda self, ctx, value: ctx.write_bytes('value', value, self.VALUE_LENGTH)
+    value_deserialize = lambda self, ctx: ctx.read_bytes('value', self.VALUE_LENGTH)
+
+    key_gethash = lambda self, key: key
+    value_gethash = lambda self, value: value
+
+
+class Test_MerbinnerTree(unittest.TestCase):
+    def test_hash(self):
+        """Manual test of the hash calculation"""
+
+        # test-the-test to ensure we understand exactly what is going on
+
+        def h(buf):
+             return hmac.HMAC(BytesBytesMerbinnerTree.HASH_HMAC_KEY, buf, hashlib.sha256).digest()
+
+        # "Empty"
+        self.assertEqual('ae135d71df652ca291b2025b06165c285aaa0f8ce9609783294d210d371ac8d9',
+                        b2x(h(x('00'))))
+
+        # "Leaf"
+        self.assertEqual('7b9f6282fa917a0c391bcf96accaad457a40fe31dcd4cdfc17e49dfba311dffb',
+                        b2x(h(x('01ffffffffdeadbeef'))))
+
+        # "Inner with left and right leaf children"
+        self.assertEqual('65ada255d463e50f569a2ac79a10772d1a3a5262deab2b3ca4bbef466abe8702',
+                        b2x(h(x('02') + h(x('01ffffffffdeadbeef')) + h(x('0100000000cafebabe')))))
+
+        # "Inner with collision on first bit"
+        self.assertEqual('6d00bce57a2178c209ab6ba3811022d98bd57d8ddc7129f4f9681de6aabc6f71',
+                        b2x(h(x('02') +
+                              h(x('02') + h(x('01ffffffffdeadbeef')) + h(x('0180000000cafebabe'))) +
+                              h(x('00')))))
+
     def test_vectors(self):
         for json_test_case in load_test_vectors('merbinnertree_hashes.json'):
-            if len(json_test_case) == 1:
-                # comment, ignore
-                continue
+            items, mode, expected_digest = json_test_case
 
-            elif len(json_test_case) == 3:
-                items, hash_func, expected_digest = json_test_case
+            items = [(x(k),x(v)) for k,v in items.items()]
+            expected_digest = x(expected_digest)
 
-                items = [(x(k),x(v)) for k,v in items.items()]
-                expected_digest = x(expected_digest)
+            mbtree = BytesBytesMerbinnerTree(items)
 
-                if hash_func == 'cat':
-                    hash_func = lambda msg: msg
+            actual_digest = None
+            if mode == 'serialize':
+                actual_digest = mbtree.serialize()
 
-                elif hash_func == 'sha256':
-                    hash_func = lambda msg: hashlib.sha256(msg).digest()
+                mbtree2 = BytesBytesMerbinnerTree.deserialize(actual_digest)
+                self.assertDictEqual(mbtree, mbtree2)
 
-                else:
-                    assert False and "invalid test: unknown hash function"
+                roundtrip_digest = mbtree2.serialize()
+                self.assertEqual(b2x(expected_digest), b2x(roundtrip_digest))
 
-                actual_digest = calc_merbinner_hash(items, hash_func=hash_func)
-
-                self.assertEqual(b2x(expected_digest), b2x(actual_digest))
+            elif mode == 'hash':
+                actual_digest = mbtree.hash
 
             else:
-                assert False and "invalid test: malformed"
+                assert False and "invalid test: unknown mode"
 
-class Test_calc_summed_merbinner_hash(unittest.TestCase):
+            self.assertEqual(b2x(expected_digest), b2x(actual_digest))
+
+
+sum_struct = struct.Struct('>H')
+class SummedBytesBytesMerbinnerTree(BytesBytesMerbinnerTree):
+    HASH_HMAC_KEY = x('e630f7a3a784d521533772d43c8874c6')
+
+    VALUE_LENGTH = 6
+
+    sum_serialize = lambda self, ctx, sum: ctx.write_bytes('sum', sum_struct.pack(sum), sum_struct.size)
+    sum_deserialize = lambda self, ctx, sum: sum_struct.unpack(ctx.read_bytes('sum', sum_struct.size))[0]
+
+    value_getsum = lambda self, value: sum_struct.unpack(value[-2:])[0]
+
+class Test_SummedMerbinnerTree(unittest.TestCase):
+    def test_hash(self):
+        """Manual test of the hash calculation"""
+
+        # test-the-test to ensure we understand exactly what is going on
+
+        def h(buf):
+             return hmac.HMAC(SummedBytesBytesMerbinnerTree.HASH_HMAC_KEY, buf, hashlib.sha256).digest()
+
+        # "Empty"
+        self.assertEqual('8e2759711e9d839f9b80e70eae03481d411350ee47ccb96f04590b954679cdfd',
+                        b2x(h(x('00'))))
+
+        # "Leaf"
+        self.assertEqual('d203a45121c5a1b3cf75931c08ed5a6ff5c7f8b574d1a98df23f99cf1fe6579b',
+                        b2x(h(x('01ffffffffdeadbeef0001'))))
+
+        # "Inner with left and right leaf children"
+        self.assertEqual('745305cfcf61e12b1c4fc28702f3db47c7166c8684ca47cabdcd90a4379819ff',
+                        b2x(h(x('02') + h(x('01ffffffffdeadbeef0001')) + x('0001') +
+                                        h(x('0100000000cafebabe0003')) + x('0003'))))
+
+        # "Inner with collision on first bit"
+        self.assertEqual('305cf686476881ebd40efb5848df0ad1da2b3bb9043c20d80c2be5fe5ec0f4b0',
+                        b2x(h(x('02') +
+                              h(x('02') + h(x('01ffffffffdeadbeef0001')) + x('0001') +
+                                          h(x('0180000000cafebabe0003')) + x('0003')) +
+                              x('0004') +
+                              h(x('00')) +
+                              x('0000'))))
+
     def test_vectors(self):
-        packer = struct.Struct('>H')
-        sum_serialize_func = lambda s: packer.pack(s)
-
         for json_test_case in load_test_vectors('summed_merbinnertree_hashes.json'):
-            if len(json_test_case) == 1:
-                # comment, ignore
-                continue
+            items, mode, (expected_digest, expected_sum)= json_test_case
 
-            elif len(json_test_case) == 3:
-                items, hash_func, (expected_digest, expected_sum)= json_test_case
+            items = [(x(k),x(v)) for k,v in items.items()]
+            expected_digest = x(expected_digest)
 
-                items = [(x(k),x(v),s) for k,(v,s) in items.items()]
-                expected_digest = x(expected_digest)
+            mbtree = SummedBytesBytesMerbinnerTree(items)
 
-                if hash_func == 'cat':
-                    hash_func = lambda msg: msg
+            actual_digest = None
+            if mode == 'serialize':
+                actual_digest = mbtree.serialize()
 
-                elif hash_func == 'sha256':
-                    hash_func = lambda msg: hashlib.sha256(msg).digest()
+                mbtree2 = SummedBytesBytesMerbinnerTree.deserialize(actual_digest)
+                self.assertDictEqual(mbtree, mbtree2)
 
-                else:
-                    assert False and "invalid test: unknown hash function"
+                roundtrip_digest = mbtree2.serialize()
+                self.assertEqual(b2x(expected_digest), b2x(roundtrip_digest))
 
-                (actual_digest, actual_sum) = calc_summed_merbinner_hash(items,
-                                                                         hash_func=hash_func,
-                                                                         sum_serialize_func=sum_serialize_func)
-
-                self.assertEqual(b2x(expected_digest), b2x(actual_digest))
-                self.assertEqual(expected_sum, actual_sum)
+            elif mode == 'hash':
+                actual_digest = mbtree.hash
 
             else:
-                assert False and "invalid test: malformed"
+                assert False and "invalid test: unknown mode"
+
+            self.assertEqual(b2x(expected_digest), b2x(actual_digest))
